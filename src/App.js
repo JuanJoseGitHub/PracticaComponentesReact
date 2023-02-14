57ca397515541b687e36e2909628aba6fb842f93
DanBaDo <byte4life@pm.me>
2023-02-14 19:44:25 +0100

    Add visula tests for Contador and Rexistro components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 import Encabezado from "./components/Encabezado/Encabezado";
 import Login from "./components/Login/Login";
 import Nova from "./components/Nova/Nova";
+import Contador from "./components/Contador/Contador"
+import Rexistro from "./components/Rexistro/Rexistro";
 
 function App() {
+
+  const novas = [
+    {
+      titulo: "Esta é unha nova",
+      contido: "Este é o contido da primeira nova"
+    },
+    {
+      titulo: "Esta é outra nova",
+      contido: "Este é o contido de outra nova"
+    }
+  ]
+
+  const componentesNovas = novas.map(
+    nova => <Nova titulo={nova.titulo} contido={nova.contido}/>
+  )
+
   return (
     <>
     <h1>Tests</h1>
 
+    <h2>Rexistro</h2>
+    <Rexistro/>
+
+    <h2>Contador</h2>
+    <Contador/>
+
     <h2>Encabezado</h2>
     <Encabezado/>
 
-    <h2>Nova</h2>
-    <Nova titulo="Este é o título" contido="Contido da nova primeira"/>
-    <Nova titulo="Este é outro título" contido="Contido da nova segunda"/>
+    <h2>Novas</h2>
+    {componentesNovas}
     </>
   );
 }
